a078014739e49fb5da018115065830457379e67a
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2017-02-24 01:22:01 +0900

    add map.html
    add map.html and change code display.js

--- www/lib/onsenui/js/display.js
+++ www/lib/onsenui/js/display.js
@@ -2,22 +2,22 @@
 
 function OnButtonClick(btn) {
         if(btn == 1 ){
-            document.location.assign("../index.html")
+            document.location.assign("map.html")
         }else if(btn == 2){
-            document.location.assign("html/law.html")
+            document.location.assign("law.html")
         }else if(btn == 3){
-            document.location.assign("html/education.html")
+            document.location.assign("education.html")
         }else if(btn == 4){
-            document.location.assign("html/science.html")
+            document.location.assign("science.html")
         }else if(btn == 5){
-            document.location.assign("html/medicine.html")
+            document.location.assign("medicine.html")
         }else if(btn == 6){
-            document.location.assign("html/engineer.html")
+            document.location.assign("engineer.html")
         }else if(btn == 7){
-            document.location.assign("html/agriculture.html")
+            document.location.assign("agriculture.html")
         }else if(btn == 8){
-            document.location.assign("html/common.html")
+            document.location.assign("common.html")
         }else{
-            document.location.assign("../index.html")
+            document.location.assign("map.html")
         }
     }
