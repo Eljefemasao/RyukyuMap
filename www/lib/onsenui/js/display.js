10981f0f0d01efdf50ee0f8e8e80145b1cc62587
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2017-02-24 02:29:32 +0900

    Change if to switch

--- www/lib/onsenui/js/display.js
+++ www/lib/onsenui/js/display.js
@@ -1,23 +1,33 @@
 //Button functions
 
 function OnButtonClick(btn) {
-        if(btn == 1 ){
-            document.location.assign("map.html")
-        }else if(btn == 2){
-            document.location.assign("law.html")
-        }else if(btn == 3){
-            document.location.assign("education.html")
-        }else if(btn == 4){
-            document.location.assign("science.html")
-        }else if(btn == 5){
-            document.location.assign("medicine.html")
-        }else if(btn == 6){
-            document.location.assign("engineer.html")
-        }else if(btn == 7){
-            document.location.assign("agriculture.html")
-        }else if(btn == 8){
-            document.location.assign("common.html")
-        }else{
-            document.location.assign("html/map.html")
+        switch (btn){
+            case 1:
+                document.location.assign("map.html");
+                break;
+            case 2:
+                document.location.assign("law.html");
+                break;
+            case 3:
+                document.location.assign("education.html");
+                break;
+            case 4:
+                document.location.assign("science.html");
+                break;
+            case 5:
+                document.location.assign("medicine.html");
+                break;
+            case 6:
+                document.location.assign("engineer.html");
+                break;
+            case 7:
+                document.location.assign("agriculture.html");
+                break;
+            case 8:
+                document.location.assign("common.html");
+                break;
+            default:
+                document.location.assign("html/map.html");
+                break;
         }
     }
