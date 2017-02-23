2385c8618f38ab806e9b475412bdebf533787d1e
KKZoo <e145704@ie.u-ryukyu.ac.jp>
2017-02-24 00:44:28 +0900

    add button function
    add JavaScript and html file

---
+++ www/lib/onsenui/js/display.js
@@ -0,0 +1,23 @@
+//Button functions
+
+function OnButtonClick(btn) {
+        if(btn == 1 ){
+            document.location.assign("../index.html")
+        }else if(btn == 2){
+            document.location.assign("html/law.html")
+        }else if(btn == 3){
+            document.location.assign("html/education.html")
+        }else if(btn == 4){
+            document.location.assign("html/science.html")
+        }else if(btn == 5){
+            document.location.assign("html/medicine.html")
+        }else if(btn == 6){
+            document.location.assign("html/engineer.html")
+        }else if(btn == 7){
+            document.location.assign("html/agriculture.html")
+        }else if(btn == 8){
+            document.location.assign("html/common.html")
+        }else{
+            document.location.assign("../index.html")
+        }
+    }
